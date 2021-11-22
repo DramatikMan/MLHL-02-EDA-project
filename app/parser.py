6b47f58c4d3c37e5a40298d3ecd05b68ace854d5
Sergey Pavlov <self@sergeypavlov.dev>
2021-11-22 23:21:37 +0300

    Move parsing logic to a dedicated class

---
+++ app/parser.py
@@ -0,0 +1,133 @@
+import re
+from datetime import date
+
+import pandas as pd
+from bs4 import BeautifulSoup
+from bs4.element import Tag
+
+
+class Parser:
+    @staticmethod
+    def get_airlines(card: Tag) -> str:
+        airlines: str = card \
+            .find(class_=re.compile('LogoImage_container')) \
+            .find('span').text.strip()
+
+        return airlines
+
+    @staticmethod
+    def get_departure_time(card: Tag) -> str:
+        departure_time: str = card \
+            .find(class_=re.compile('LegInfo_routePartialDepart')) \
+            .find(class_=re.compile('LegInfo_routePartialTime')) \
+            .find('span').text.strip()
+
+        return departure_time
+
+    @staticmethod
+    def get_arrival_time(card: Tag) -> str:
+        arrival_time: str = card \
+            .find(class_=re.compile('LegInfo_routePartialArrive')) \
+            .find(class_=re.compile('LegInfo_routePartialTime')) \
+            .find('span').text.strip()
+
+        return arrival_time
+
+    @staticmethod
+    def get_duration(card: Tag) -> int:
+        duration_text: str = card \
+            .find(class_=re.compile('Duration_duration')) \
+            .text.strip()
+
+        matches: list[str] = re.findall(r'\d+(?=\s)', duration_text)
+        duration_in_minutes = int(matches[0]) * 60 \
+            + int(matches[1] if len(matches) == 2 else 0)
+
+        return duration_in_minutes
+
+    @staticmethod
+    def get_departure_airport(card: Tag) -> str:
+        departure_airport: str = card \
+            .find(class_=re.compile('LegInfo_routePartialDepart')) \
+            .find(class_=re.compile('LegInfo_routePartialCityTooltip')) \
+            .text.strip()
+
+        return departure_airport
+
+    @staticmethod
+    def get_arrival_airport(card: Tag) -> str:
+        arrival_airport: str = card \
+            .find(class_=re.compile('LegInfo_routePartialArrive')) \
+            .find(class_=re.compile('LegInfo_routePartialCityTooltip')) \
+            .text.strip()
+
+        return arrival_airport
+
+    @staticmethod
+    def get_min_price(card: Tag) -> int:
+        min_price: str = card \
+            .find(class_=re.compile('Price_mainPriceContainer')) \
+            .find('span').text.strip()
+
+        min_price = re.sub(r'\D', '', min_price)
+
+        return int(min_price)
+
+    @staticmethod
+    def get_stops_count(card: Tag) -> int:
+        stops_count_text: str = card \
+            .find(class_=re.compile('LegInfo_stopsLabelContainer')) \
+            .find('span').text.strip()
+
+        if re.match(r'\d', stops_count_text) is not None:
+            stops_count = int(stops_count_text.split(' ')[0])
+        else:
+            stops_count = 0
+
+        return stops_count
+
+    @classmethod
+    def get_dataframe(
+        cls,
+        destination: str,
+        on_date: date,
+        HTML: str
+    ) -> pd.DataFrame:
+        soup = BeautifulSoup(HTML, 'html.parser')
+
+        column_x_getter = {
+            'airlines': cls.get_airlines,
+            'departure_time': cls.get_departure_time,
+            'arrival_time': cls.get_arrival_time,
+            'duration_m': cls.get_duration,
+            'departure_airport': cls.get_departure_airport,
+            'arrival_airport': cls.get_arrival_airport,
+            'min_price': cls.get_min_price,
+            'stops_count': cls.get_stops_count
+        }
+
+        df = pd.concat(
+            [
+                pd.DataFrame(
+                    [[getter(child) for getter in column_x_getter.values()]],
+                    columns=column_x_getter.keys()
+                )
+                for child in soup.children
+                if not (
+                    child.has_attr('id') or
+                    child.has_attr('class') or
+                    child.find(class_=re.compile('InlineBrandBanner_link'))
+                )
+            ],
+            ignore_index=True
+        )
+
+        today: date = date.today()
+        df['destination'] = destination
+        df['parsing_date'] = today
+        df['departure_date'] = on_date
+        df['days_until'] = (on_date - today).days
+
+        df = df.reindex(columns=[*df.columns[-4:], *df.columns[:-4]])
+
+        return df
