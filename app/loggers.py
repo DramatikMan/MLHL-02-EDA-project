b92a656b8866668dba18ab72cc83e34a814dc92c
Sergey Pavlov <self@sergeypavlov.dev>
2021-11-28 12:46:01 +0300

    Update logging setup

--- app/loggers.py
+++ app/loggers.py
@@ -1,23 +1,39 @@
 import logging
+import os
 import sys
+
+
+LOG_PATH = f'{os.environ["PWD"]}/runner.log'
 
 
 # main runner script events logging
 main_task_log = logging.getLogger('main_task_log')
 main_task_log.setLevel(logging.INFO)
-main_task_log_stream_handler = logging.StreamHandler(stream=sys.stdout)
-main_task_log_stream_handler.setFormatter(fmt=logging.Formatter(
+main_task_log_formatter = logging.Formatter(
     fmt='[%(levelname)s]:[%(asctime)s] %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
-))
-main_task_log.addHandler(hdlr=main_task_log_stream_handler)
+)
+# stdout output handler
+main_task_log_stdout_handler = logging.StreamHandler(stream=sys.stdout)
+main_task_log_stdout_handler.setFormatter(fmt=main_task_log_formatter)
+main_task_log.addHandler(hdlr=main_task_log_stdout_handler)
+# file output handler
+main_task_log_file_handler = logging.FileHandler(filename=LOG_PATH)
+main_task_log_file_handler.setFormatter(fmt=main_task_log_formatter)
+main_task_log.addHandler(hdlr=main_task_log_file_handler)
 
 # scraper runs logging
 scraping_log = logging.getLogger('scraping_log')
 scraping_log.setLevel(logging.INFO)
-scraping_stream_handler = logging.StreamHandler(stream=sys.stdout)
-scraping_stream_handler.setFormatter(fmt=logging.Formatter(
-    fmt='[%(levelname)s]:[%(asctime)s] [%(city)s]:[%(date)s] %(message)s',
+scraping_log_formatter = logging.Formatter(
+    fmt='[%(levelname)s]:[%(asctime)s] [%(city)s @ %(date)s] %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
-))
-scraping_log.addHandler(hdlr=scraping_stream_handler)
+)
+# stdout output handler
+scraping_log_stdout_handler = logging.StreamHandler(stream=sys.stdout)
+scraping_log_stdout_handler.setFormatter(fmt=scraping_log_formatter)
+scraping_log.addHandler(hdlr=scraping_log_stdout_handler)
+# file output handler
+scraping_log_file_handler = logging.FileHandler(filename=LOG_PATH)
+scraping_log_file_handler.setFormatter(fmt=scraping_log_formatter)
+scraping_log.addHandler(hdlr=scraping_log_file_handler)
--- app/notify.py
+++ app/notify.py
@@ -2,25 +2,32 @@ import telegram
 import os
 import sys
 
+from app.loggers import LOG_PATH
+
 
 CHAT_ID = -680917160
 
 
 def main() -> None:
     bot = telegram.Bot(token=os.environ['TG_TOKEN'])
+
     try:
         caption = 'Scraping succeeded.' if sys.argv[1] == '0' \
             else 'Scraping failed.'
-        bot.send_document(
-            chat_id=CHAT_ID,
-            caption=caption,
-            document=open('runner.log')
-        )
+        document = open(LOG_PATH)
     except Exception:
         bot.send_message(
             chat_id=CHAT_ID,
             text='Failed to get scraping status and/or logs.'
         )
+    else:
+        bot.send_document(
+            chat_id=CHAT_ID,
+            caption=caption,
+            document=document
+        )
+        os.truncate(LOG_PATH, length=0)
+        document.close()
 
 
 if __name__ == '__main__':
